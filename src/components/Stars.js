d5175a881d332d49d1c27acee34db2674d61fef9
Alex Kelley <adkelley@mac.com>
2019-10-02 13:35:27 -0700

    update: react-native 61

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -6,11 +6,12 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Stars = ({rating}) => {
   const stars = [...Array(Math.ceil(rating))];
+  const fullStars = Math.floor(rating);
 
   return (
     <View style={{flexDirection: 'row'}}>
       {stars.map((_, i) => {
-        const name = Math.floor(rating) > i ? 'star' : 'star-half';
+        const name = fullStars > i ? 'star' : 'star-half';
         return <Icon key={i} name={name} color="#FFD64C" />;
       })}
     </View>
